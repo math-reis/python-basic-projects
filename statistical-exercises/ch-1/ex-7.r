959b2c5aeea05fa8dc9e56d636f3424221ef6ca1
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-05 16:18:37 -0300

    Create ex-7.r

---
+++ statistical-exercises/ch-1/ex-7.r
@@ -0,0 +1,2 @@
+# 7. We note that a simple histogram provides a visual glimpse into how data are distributed. 
+# Since we have heard about the normal bell-curve distribution, is it possible to see a histogram of that?
