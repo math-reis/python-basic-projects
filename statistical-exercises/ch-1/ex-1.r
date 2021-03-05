89d527094b2537e8939f17d75c872e36559e6e05
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-05 13:12:44 -0300

    Update ex-1.r

--- statistical-exercises/ch-1/ex-1.r
+++ statistical-exercises/ch-1/ex-1.r
@@ -1,6 +1,22 @@
 # 1. Using R, answer the following questions.
+#############################################
 
 # (a) The sum of 137 and 242.
 
 
 # (b) The difference between 1,206 and 373.
+
+
+# (c) The product of 547 and 23.  
+
+
+# (d) Divide 8,840 by 17.  
+
+
+# (e) Raise 11 to the 3rd power.  
+
+
+# (f) Find the square root of 64.  
+
+
+# (g) Find the cube root of 8,000.
