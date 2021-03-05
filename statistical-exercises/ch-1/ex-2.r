5b84cd510ee3cc772521e0ca5733302e33960df4
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-05 13:31:51 -0300

    Create ex-2.r

---
+++ statistical-exercises/ch-1/ex-2.r
@@ -0,0 +1,7 @@
+# 2. Enter the following small data set directly into the R Workspace, and name it E1_1: 81, 17, 7, 55, 2, 98, 71, 47, 19, 8, 3, 10, 28, 65, 80. Check to make sure that E1_1 contains these elements, and answer the following questions.
+
+#Comment1. Use the c() function to create object E1_1.
+E1_1 <- c(81, 17, 7, 55, 2, 98, 71, 47, 19, 8, 3, 10, 28, 65, 80)
+#Comment2. Examine contents of E1_1.
+E1_1
+## [1] 81 17 7 55 2 98 71 47 19 8 3 10 28 65 80
