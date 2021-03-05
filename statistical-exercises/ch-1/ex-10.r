cd6007a204d3004379101245b14e9b5c0cf713e7
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-05 16:21:19 -0300

    Create ex-10.r

---
+++ statistical-exercises/ch-1/ex-10.r
@@ -0,0 +1,13 @@
+# 10. Answer the following questions concerning the data frame E1_3. 
+
+# (a) Find the summary statistics (the mean, the median, the maximum, the minimum, the first and third quartiles) of the variable Population.
+
+
+
+# (b) Find the summary statistics (the mean, the median, the maximum, the minimum, the first and third quartiles) of the variable GDP.
+
+
+
+# (c) Find the summary statistics (the mean, the median, the maximum, the minimum, the first and third quartiles) of the variable Percent Urban.
+
+
