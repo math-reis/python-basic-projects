3fc535c2f0bd11edf55d7177f0e9f71c1d510676
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-05 16:19:10 -0300

    Create ex-8.r

---
+++ statistical-exercises/ch-1/ex-8.r
@@ -0,0 +1,3 @@
+# 8. Should we be very confident that these data really are good representations of 
+# the actual water level of Lake Huron over the period from 1875 to 1972? What might 
+# be uncontrolled influences on the measurements that are taken each year?
