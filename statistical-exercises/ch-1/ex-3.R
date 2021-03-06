a90196bc1500a5c153942c0b334b36de12fd3078
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-06 12:51:10 -0300

    Update ex-3.R

--- statistical-exercises/ch-1/ex-3.R
+++ statistical-exercises/ch-1/ex-3.R
@@ -4,11 +4,14 @@ E1_1 <- c(81, 17, 7, 55, 2, 98, 71, 47, 19, 8, 3, 10, 28, 65, 80)
 x <- sort(E1_1)
 
 # Long path
-sum(x)
+a <- sum(x)
+a
 ## [1] 591
-length(x)
+b <- length(x)
+b
 ## [1] 15
-591 / 15
+mean <- a / b
+mean
 ## [1] 39.4
 
 # Short path
