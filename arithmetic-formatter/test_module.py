2e663cf6a20e21be30c3f4b123055ce5f024675e
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-08 19:27:56 -0300

    Update test_module.py

--- arithmetic-formatter/test_module.py
+++ arithmetic-formatter/test_module.py
@@ -1,7 +1,6 @@
 import unittest
 from arithmetic_arranger import arithmetic_arranger
 
-# the test case
 class UnitTests(unittest.TestCase):
     def test_arrangement(self):
         actual = arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"])
