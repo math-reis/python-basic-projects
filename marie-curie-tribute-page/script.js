c0fc2af8ee647d355bba30e26b2bbddf3b7f39da
Matheus Reis <mgrp.ufrgs@gmail.com>
2021-03-16 14:50:00 -0300

    Create script.js

---
+++ marie-curie-tribute-page/script.js
@@ -0,0 +1,46 @@
+<nav>
+  <ul>
+    <li><a href="#about">About</a></li>
+    <li><a href="#life">Life and work</a></li>
+    
+  </ul>
+</nav>
+
+<section id="about">
+  <div>
+    <h1>Marie Curie</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, reiciendis doloremque vel porro natus animi mollitia amet omnis hic adipisci exercitationem quos neque minima beatae esse nulla! Voluptatem, quas corporis.</p>
+      <p>Marie Curie lalala...</p>
+  </div>
+  <div>
+    <img id="img-div" src="https://images.newscientist.com/wp-content/uploads/2019/07/07114117/gettyimages-50693712.jpg" alt="Marie Curie">
+    <p id="img-caption">Marie Curie in her lab.</p>
+  </div>
+</section>
+
+<section id="#life">
+  <h2>Most Important Works</h2>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+
+
+
+
+
+
+
+
+
