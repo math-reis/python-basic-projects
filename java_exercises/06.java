22f42c5d2effbb358279f7b50686e2249e1e673a
Matheus Reis <math.reis.dev@gmail.com>
2021-09-07 12:45:30 -0300

    Update 06.java

--- java_exercises/06.java
+++ java_exercises/06.java
@@ -5,18 +5,17 @@ class Main
   public static void main (String[]args)
   {
     Scanner num = new Scanner (System.in);
-    System.out.println ("Enter a number: ");
+    System.out.print ("Enter a number: ");
     int n = num.nextInt ();
 
     Scanner option = new Scanner (System.in);
-    System.out.println ("Choose between the sum or product: [S/P]");
+    System.out.print ("Choose between the sum or product: [S/P]");
     char op = option.next ().charAt (0);
 
     if (op == 's' || op == 'S')
       {
 	int sum = ((n + 1) * n) / 2;
-	String result =
-	String.format ("The sum of the numbers 1 to %d is %d.", n, sum);
+	String result = String.format ("The sum of the numbers 1 to %d is %d.", n, sum);
 	System.out.println (result);
       }
     if (op == 'p' || op == 'P')
@@ -26,8 +25,7 @@ class Main
 	  {
 	    fact = fact * i;
 	  }
-	String result =
-	String.format ("The product of the numbers 1 to %d is %d.", n, fact);
+	String result = String.format ("The product of the numbers 1 to %d is %d.", n, fact);
 	System.out.println (result);
       }
     else
