ae7bbc6039b7b0fc16293e21afc418dfdd6718a9
Matheus Reis <math.reis.dev@gmail.com>
2021-09-07 07:25:54 -0300

    Update 02.java

--- java_exercises/02.java
+++ java_exercises/02.java
@@ -1,11 +1,14 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner userName = new Scanner(System.in); 
-    System.out.println("Enter your name: ");
+class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner userName = new Scanner (System.in);
+      System.out.println ("Enter your name: ");
 
-    String name = userName.nextLine();
-    System.out.println("Username is: " + name);
+    String name = userName.nextLine ();
+      System.out.println ("Username is: " + name);
   }
 }
+
