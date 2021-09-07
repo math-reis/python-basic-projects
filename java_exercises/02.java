e633f5cb49656030cbfda2177177d2cfb0a548ff
Matheus Reis <math.reis.dev@gmail.com>
2021-09-07 12:42:33 -0300

    Update 02.java

--- java_exercises/02.java
+++ java_exercises/02.java
@@ -5,8 +5,7 @@ class Main
   public static void main (String[]args)
   {
     Scanner userName = new Scanner (System.in);
-    System.out.println ("Enter your name: ");
-
+    System.out.print ("Enter your name: ");
     String name = userName.nextLine ();
     System.out.println ("Username is: " + name);
   }
