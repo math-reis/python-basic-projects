60990de271576b938b8faddcaa31c641b41a940d
Matheus Reis <math.reis.dev@gmail.com>
2021-09-07 07:23:46 -0300

    Create 02.java

---
+++ java_exercises/02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+class Main {
+  public static void main(String[] args) {
+    Scanner userName = new Scanner(System.in); 
+    System.out.println("Enter your name: ");
+
+    String name = userName.nextLine();
+    System.out.println("Username is: " + name);
+  }
+}
