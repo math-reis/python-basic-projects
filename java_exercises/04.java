7ed1bde002dfe8b68f66b04bd67b2b84e46b4ffb
Matheus Reis <math.reis.dev@gmail.com>
2021-09-07 12:45:47 -0300

    Update 04.java

--- java_exercises/04.java
+++ java_exercises/04.java
@@ -5,7 +5,7 @@ class Main
   public static void main (String[]args)
   {
     Scanner number = new Scanner (System.in);
-    System.out.print("Enter a number: ");
+    System.out.print ("Enter a number: ");
     int n = number.nextInt ();
     int sum = ((n + 1) * n) / 2;
     String result = String.format ("The sum of the numbers 1 to %d is %d.", n, sum);
