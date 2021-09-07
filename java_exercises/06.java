07baf39971c2a3885a23d4603461ad6ec280a53d
Matheus Reis <math.reis.dev@gmail.com>
2021-09-07 09:55:32 -0300

    Create 06.java

---
+++ java_exercises/06.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner num = new Scanner (System.in);
+      System.out.println ("Enter a number: ");
+    int n = num.nextInt ();
+
+    Scanner option = new Scanner (System.in);
+      System.out.println ("Choose between the sum or product: [S/P]");
+    char op = option.next ().charAt (0);
+
+
+    if (op == 's' || op == 'S')
+      {
+	int sum = ((n + 1) * n) / 2;
+	String result =
+	  String.format ("The sum of the numbers 1 to %d is %d.", n, sum);
+	  System.out.println (result);
+      }
+    if (op == 'p' || op == 'P')
+      {
+	int i, fact = 1;
+	int number = 5 for (i = 1; i <= number; i++)
+	  {
+	    fact = fact * i;
+
+	    System.out.println (fact);
+	  }
+	else
+	{
+	  System.out.println ("Insert a valid option.");
+	}
+      }
+  }
